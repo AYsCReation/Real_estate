d85c04eeaddcc25836a2114140fcc972aa5d2e9d
AYsCReation <ayushm850@gmail.com>
2023-12-12 18:56:53 +0530

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	new file:   src/pages/About.jsx 	new file:   src/pages/Home.jsx 	new file:   src/pages/Profile.jsx 	new file:   src/pages/SignOut.jsx 	new file:   src/pages/Signin.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {BrowserRouter , Routes , Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import SignOut from './pages/SignOut';
+import Profile from './pages/Profile';
+import About from './pages/About';
 
 const App = () => {
-  return (
-    <h1 className='text-red-500 text-center'>App</h1>
-  )
+  return <BrowserRouter>
+  <Routes>
+  <Route path='/' element={<Home />} />
+  <Route path='/sign-in' element={<Signin />} />
+  <Route path='/sign-up' element={<SignOut />} />
+  <Route path='/profile' element={<Profile />} />
+  <Route path='/About' element={<About />} />
+  </Routes>
+  </BrowserRouter>;
 }
 
 export default App
