bdf610a9414db36fa53ce57eb84f2945929206de
AYsCReation <ayushm850@gmail.com>
2023-12-13 20:41:50 +0530

    Changes to be committed: 	modified:   .gitignore 	modified:   api/index.js 	modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect("mongodb+srv://ayushm850:ayushmishra@cluster0.qws2kwa.mongodb.net/?retryWrites=true&w=majority")
+.then(() =>{
+    console.log('connected to mongodb!');
+})
+.catch((err) =>{
+    console.log("not connected" , err);
+})
 const app = express();
 
 app.listen(3000, () =>{
