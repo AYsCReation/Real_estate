ba9e80cb05e4dc27f2d24c7003637bdfc988b33e
AYsCReation <ayushm850@gmail.com>
2023-12-15 19:12:41 +0530

    signup

--- api/index.js
+++ api/index.js
@@ -1,18 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import router from "./routes/auth.route.js";
+
 dotenv.config();
 
 mongoose.connect("mongodb+srv://ayushm850:ayushmishra@cluster0.qws2kwa.mongodb.net/?retryWrites=true&w=majority")
-.then(() =>{
+  .then(() => {
     console.log('connected to mongodb!');
-})
-.catch((err) =>{
-    console.log("not connected" , err);
-})
-const app = express();
+  })
+  .catch((err) => {
+    console.log("not connected", err);
+  });
 
-app.listen(3000, () =>{
-    console.log('Server is running on port 3000');
-})
- 
+const app = express();
+app.use(express.json());
+
+app.use("/api/auth", router );
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
