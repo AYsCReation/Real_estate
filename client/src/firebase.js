b7a5ccd7d7f60ff4c655138cf3a0c990cd9ea1c3
AYsCReation <ayushm850@gmail.com>
2023-12-29 00:12:46 +0530

    google authentication

---
+++ client/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmPt5qhjTMlYxRYE8bmF1QuyhuHH_8JJw",
+  authDomain: "aysestate-1f949.firebaseapp.com",
+  projectId: "aysestate-1f949",
+  storageBucket: "aysestate-1f949.appspot.com",
+  messagingSenderId: "962805816511",
+  appId: "1:962805816511:web:3da4514ac66efe98a53a0a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
