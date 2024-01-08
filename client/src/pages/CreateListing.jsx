4ca5d2beb304216328fa005809ecc09af9581a9a
AYsCReation <ayushm850@gmail.com>
2024-01-09 00:29:24 +0530

    create listing functionality done

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,64 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
+import app from '../firebase';
 const CreateListing = () => {
+  const [files , setFiles] = useState([]);
+  const [formData , setFormData] = useState({
+    imageUrls:[],
+  });
+  const [imageUploadError , setImageUploadError] = useState(false);
+  const [loading , setLoading] = useState(false);
+  const handleChangeImage = () =>{
+    if(files.length + formData.imageUrls.length == 0) setImageUploadError('First Select the images Champ!');
+    else if(files.length > 0 && files.length + formData.imageUrls.length <7){
+      setLoading(true);
+      setImageUploadError(false);
+   
+    const Promises = [];
+    for(let i = 0 ; i < files.length ; i++){
+      Promises.push(handleStorage(files[i]));
+    }
+    Promise.all(Promises).then((urls) =>{
+      setFormData({...formData , imageUrls : formData.imageUrls.concat(urls) });
+      setImageUploadError(false);
+      setLoading(false);
+    })
+    .catch((error) =>{
+      setImageUploadError('Getting error on uploading the Images! (max 2mb per image)');
+      setLoading(false);
+    })
+  }else{
+    setImageUploadError('More than 6 Image can be a trouble , try less than 6');
+  }
+}
+  const handleStorage = async(file) =>{
+    return new Promise((resolve, reject) =>{
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage , fileName);
+        const uploadTask = uploadBytesResumable(storageRef , file);
+        uploadTask.on('state_changed' , 
+        (snapshot) =>{
+          const progress = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+        },
+        (error) =>{
+          reject(error);
+
+        },
+        () =>{
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>   {
+          resolve(downloadURL);
+        })
+      }
+        )
+    })
+  }
+  const handleDeleteImage = (index) =>{
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_ , i) => i !== index),
+    })
+  }
   return (
     <main className='max-w-4xl mx-auto p-3'>
       <h1 className='text-3xl font-semibold text-center my-7'>Create a Listing</h1>
@@ -61,9 +119,17 @@ const CreateListing = () => {
           <span className='font-normal ml-2 text-gray-600'> The first image will be the cover (max 6)</span>
           </p>
           <div className='flex gap-4'>
-            <input className='p-3 border border-gray-300 rounded w-full' type="file" accept='image/*' multiple />
-            <button className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>Upload</button>
+            <input onChange={(e) => setFiles(e.target.files)}  className='p-3 border border-gray-300 rounded w-full' type="file" accept='image/*' multiple />
+            <button disabled = {loading} type='button' onClick={handleChangeImage} className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-50'>{loading ? 'Uploading...' : 'upload'}</button>
+           
           </div>
+          <p className='text-red-700'>{imageUploadError && imageUploadError}</p>
+          {formData.imageUrls.length > 0 && formData.imageUrls.map((url , index) =>(
+           <div key={url} className='flex justify-between p-3 border items-center'> <img src={url} alt='Listing Image' className='w-20 h-20 object-contain rounded-lg' />
+         <button type='button' onClick={()=> handleDeleteImage(index)} className='text-red-700 p-3 hover:opacity-75'>DELETE</button>
+          </div>
+          ))
+          }
           <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Create List</button>
         </div>
        
